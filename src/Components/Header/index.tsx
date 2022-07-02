55947a605dbee604fbc3b32fe7b6340b46ea01a1
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2022-07-02 09:01:32 -0300

    :sparkles: Created home

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -6,7 +6,13 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="ig.news" width={100} height={100} />
+        <Image
+          layout="fixed"
+          src="/images/logo.svg"
+          alt="ig.news"
+          width={100}
+          height={100}
+        />
         <nav>
           <a href="" className={styles.active}>
             Home
